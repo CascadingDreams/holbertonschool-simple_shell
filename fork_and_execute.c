dc5b3d083f6479f989f7b11ffe4257838e7bbef2
Sammy <myallio94@gmail.com>
2025-04-22 13:43:01 +0000

    added new error to fork and execute

--- fork_and_execute.c
+++ fork_and_execute.c
@@ -3,14 +3,16 @@
 /**
  * fork_and_execute - Forks a new process and executes the command.
  * @input_line: the command string.
+ * @envp: environ
+ *
+ * Return: void
  */
 
 void fork_and_execute(char *input_line, char **envp)
 {
 	pid_t pid;
 	int status;
-	char *full_path;
-	char *argv[MAX_ARGS];
+	char *full_path, *argv[MAX_ARGS];
 
 	parse_arguments(input_line, argv);
 	if (!argv[0])
@@ -19,25 +21,25 @@ void fork_and_execute(char *input_line, char **envp)
 	full_path = find_command_path(argv[0], envp);
 	if (!full_path)
 	{
-		fprintf(stderr, "%s: command not found\n", argv[0]);
+		fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
+		if (!isatty(STDIN_FILENO))
+			exit(127);
 		return;
 	}
 
 	pid = fork();
-	if (pid < 0)
-	{
-		perror("Error: fork failed");
-		fprintf(stderr, "Failed to create a new process. Exiting...\n");
-		free(full_path);
-		exit(1);
-	}
-
 	if (pid == 0)
 	{
 		execve(full_path, argv, envp);
 		perror("execve failed");
 		free(full_path);
 		exit(127);
+	}
+	else if (pid < 0)
+	{
+		perror("fork failed");
+		free(full_path);
+		exit(1);
 	}
 	else
 	{
