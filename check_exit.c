25b79657070da776f10ff83b071eafeafa7c41f2
User10538 <10538@holbertonstudents.com>
2025-04-24 08:10:28 +0000

    Upload file to save

--- check_exit.c
+++ check_exit.c
@@ -7,9 +7,24 @@
  */
 void check_exit_builtin(char *input)
 {
-    if (strcmp(input, "exit") == 0)
-    {
-        free(input); /* free memory before exiting*/
-        exit(EXIT_SUCCESS);
+	size_t len;
+	int exit_code;
+
+	if (input == NULL)
+		return;
+
+	len = strlen(input);
+
+	if (len > 0 && input[len - 1] == '\n')
+		input[len - 1] = '\0';
+	
+	if (strcmp(input, "exit") == 0)
+	{
+		exit_code = EXIT_SUCCESS;
+
+		/*check for space and number after exit*/
+
+		free(input); /* free memory before exiting*/
+		exit(exit_code);
     }
 }
